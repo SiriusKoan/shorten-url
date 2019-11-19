7bc529af21f515ed77ff9fcab20b94647304cee6
SiriusKoan <k.peihsun@gmail.com>
2019-11-19 16:03:42 +0800

    for test

--- app.py
+++ app.py
@@ -5,22 +5,24 @@ from os import system
 app = Flask(__name__)
 CORS(app)
 
-file = open('.num.txt', 'r')
-num = file.read()
-num = int(num)
-file.close()
+with open('.num.txt', 'r') as num_file:
+    read = num_file.read()
+    num = int(read)
+
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = open('.num.txt', 'w')
-        file.write(str(num+1))
         url = request.form.get('url')
+        with open('.num.txt', 'w') as write:
+            write.write(str(num+1))
+
         system('touch ' + str(num+1) + '.html')
+
         model = open('model.html').read()
         model = model.replace('url', url)
         command = 'echo \'%s\' > %s'%(model, str(num+1))
         system(command)
-        file.close()
 
 
     return render_template('index.html', path = 'https://cnmc.tw/s/' + str(num+1))
