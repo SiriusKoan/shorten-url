aa6658cc55d11f694b0ee2fae14578402b5df43c
CSY54 <alex35748954@gmail.com>
2019-12-05 15:56:07 +0800

    Modify for readability

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from os import system, popen
 from flask_recaptcha import ReCaptcha
 from models import *
+from string import ascii_letters, digits
 
 
 app = Flask(__name__)
@@ -19,33 +20,33 @@ app.config.update(dict(
 recaptcha = ReCaptcha()
 recaptcha.init_app(app)
 
-num = [str(i) for i in range(10)]
-alpha = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
-allow = num + alpha + ["_", "-", "\"]
+allow = ascii_letters + digits + '_-\\'
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html', path = '')
-    if request.method == 'POST':
+    elif request.method == 'POST':
         if recaptcha.verify():
             url = request.form.get('url')
             new_url = request.form.get('new-url')
-            for s in list(new_url):
-                if s not in allow:
-                       return "You use not allowed character..."
+
+            if not all(ch in allow for ch in new_url):
+                return "New URL not allowed..."
                       
             trans(url, new_url)
 
             model = open('model.html').read()
             model = model.replace('url', url)
-            command = 'touch ../s/' + new_url + '&& echo \'%s\' > ../s/%s'%(model, new_url)
+            command = 'touch ../s/' + new_url + ' && echo \'%s\' > ../s/%s'%(model, new_url)
             system(command)
 
             return 'https://cnmc.tw/s/' + new_url
         else:
             ###################print(request.path)
             return '<h1 style="color: red;">please verify</h1><br><a href="/">back home</a>'
+
 
 @app.errorhandler(404)
 def redirect():
